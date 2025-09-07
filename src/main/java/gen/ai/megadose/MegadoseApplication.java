28ffd2ee3f0424aa67d538334ae030eb1186a40e
jinwoo.kim <luziatcode@gmail.com>
2025-09-07 17:49:22 +0900

    init springboot

---
+++ src/main/java/gen/ai/megadose/MegadoseApplication.java
@@ -0,0 +1,13 @@
+package gen.ai.megadose;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MegadoseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MegadoseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gen/ai/megadose/MegadoseApplicationTests.java
@@ -0,0 +1,13 @@
+package gen.ai.megadose;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MegadoseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
